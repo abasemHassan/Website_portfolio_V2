136a940f6387181a976b56e594aeb43151f8325b
abasemHassan <61631366+abasemHassan@users.noreply.github.com>
2025-02-19 19:52:46 -0500

    Minor edits

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -27,7 +27,7 @@ function Header() {
       <section id="header" className="scroll-mt-24">
       <div className="flex items-center fixed w-full justify-between border-b-[1px] bg-white z-50">
         <div className="w-[90px] h-[90px] bg-black ">
-          <img src="/logo.png" alt="Logo" />
+          <img src="logo.png" alt="Logo" />
         </div>
         <div className="hidden md:flex gap-14 ">
           {menu.map((item) => (
@@ -41,7 +41,7 @@ function Header() {
           ))}
         </div>
         <div className="w-[90px] h-[90px] bg-red-500 flex justify-center items-center">
-          <a href="PDF/_AmrHassanResumeUS.pdf">
+          <a href="_AmrHassanResumeUS.pdf">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="white">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 13v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h5m7-4h4m0 0v4m0-4L10 14"/>
             </svg>
