1f6a2b30a3e335e6722ea86b0088a939663ac3c5
abasemHassan <61631366+abasemHassan@users.noreply.github.com>
2025-02-19 19:56:01 -0500

    Update ProfilePic.tsx

--- src/Components/ProfilePic.tsx
+++ src/Components/ProfilePic.tsx
@@ -3,7 +3,7 @@
 function ProfilePic() {
   return (
     <div className='fixed mt-[90px]'>
-        <img src="/user-image.png"
+        <img src="user-image.png"
         className='h-screen object-cover w-screen'/>
     </div>
   )
