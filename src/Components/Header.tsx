d858aec403107c9de9979c30b2cca7f422107190
abasemHassan <61631366+abasemHassan@users.noreply.github.com>
2025-02-20 18:53:55 -0500

    Updated pdf and included it in the folder

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -41,7 +41,7 @@ function Header() {
           ))}
         </div>
         <div className="w-[90px] h-[90px] bg-red-500 flex justify-center items-center">
-          <a href="_AmrHassanResumeUS.pdf">
+          <a href="AmrHassanResumeUS.pdf">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="white">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 13v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h5m7-4h4m0 0v4m0-4L10 14"/>
             </svg>
